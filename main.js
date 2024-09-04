857e78cc7a7008c1f87b362782d6ced687771545
zerrodigital <zerrodigital@gmail.com>
2024-09-04 22:27:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-//load custom popup
+// Custom Popup Script
+// This script adds a popup feature to the site.
+// To integrate it, include the following in your Shopify theme:
+// <script src="https://raw.githubusercontent.com/zerrodigital/poppay/main/main.js" defer></script>
 
+// Initialize the custom popup functionality
+function initPopup() {
+    // Logic for popup behavior
+}
